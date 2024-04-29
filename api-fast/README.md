26080daee225d4ae3cad788b33bdeb1c1fd07417
n00b St4r <realgoldcraft@gmail.com>
2024-04-29 17:38:10 +0700

    move aimodule to a new repo

--- api-fast/README.md
+++
@@ -1,3 +0,0 @@
-# Installation (Pemasangan)
-* Pasang terlebih dahulu python versi 3.12
-* Jalankan perintah `pip install -r requirements.txt`
--- api-fast/caller.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-import subprocess
-from AI import main
-
-
-def open_py_file(link, questions, choices, answers):
-    main(link, questions, choices, answers)
-    
-# open_py_file()
